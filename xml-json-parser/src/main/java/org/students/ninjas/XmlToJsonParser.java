3bffcac65a50312e0e36f14c616c0f36849733db
Miroslav Vetrov <miroslav.vetrov@endava.com>
2023-10-31 18:42:11 +0200

    Xml to Json parser

---
+++ xml-json-parser/src/main/java/org/students/ninjas/XmlToJsonParser.java
@@ -0,0 +1,35 @@
+package org.students.ninjas;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class XmlToJsonParser {
+
+    private static final String RESOURCES_PATH = "xml-json-parser/src/main/resources/";
+
+    public static void main(String[] args) throws Exception {
+        File xmlDirectory = new File(RESOURCES_PATH + "xmls/");
+        File jsonDirectory = new File(RESOURCES_PATH + "jsons/");
+        while (true) {
+            Thread.sleep(5000);
+            convertXMLtoJSON(xmlDirectory, jsonDirectory);
+        }
+    }
+
+    private static void convertXMLtoJSON(File xmlDirectory, File jsonDirectory) throws IOException {
+        List<File> XMLs = Arrays.stream(Objects.requireNonNull(xmlDirectory.listFiles())).toList();
+        XmlMapper xmlMapper = new XmlMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        for (File XML : XMLs) {
+            Map<String, Object> map = xmlMapper.readValue(XML, Map.class);
+            objectMapper.writeValue(new File(jsonDirectory + "/" + XML.getName().replace(".xml", ".json")), map);
+        }
+    }
+}
